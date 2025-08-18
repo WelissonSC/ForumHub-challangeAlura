50d1f01e36d8104f8f34501e2ee7c6f9836c546b
WelissonSC <welissonsilvacunha@gmail.com>
2025-08-18 10:02:35 -0300

    first commit

---
+++ src/main/java/com/welisson/challangeAlura_FotumHub/ChallangeAluraFotumHubApplication.java
@@ -0,0 +1,13 @@
+package com.welisson.challangeAlura_FotumHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeAluraFotumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeAluraFotumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welisson/challangeAlura_FotumHub/ChallangeAluraFotumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welisson.challangeAlura_FotumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeAluraFotumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
